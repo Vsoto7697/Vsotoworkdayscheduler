6b76d01b32d22df1fca6d382196ae30767474c70
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-10-11 19:51:01 -0400

    Sets the hour id and description value in local storage when the save button is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 //adds current day and date in real time that allows user to know what day it is
 $("#currentDay").text(moment().format('dddd, MMMM Do'));
+
+//Sets the hour id and description value in local storage when the save button is clicked.
+$(".saveButton").on("click", function() {
+    var hour = $(this).parent().attr("id");
+    var description = $(this).siblings(".time-stamp-description").val();
+    localStorage.setItem(hour, time-stamp-description);
+    })
